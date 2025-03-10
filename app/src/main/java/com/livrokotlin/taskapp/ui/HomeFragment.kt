02288274e8b0af61b206573ae9f5575e8cb4d72e
vittorugo - pc 2 <hgloureirocosta@gmail.com>
2025-03-10 16:39:55 -0300

    Adicionado logout na autenticação do usuario.

--- app/src/main/java/com/livrokotlin/taskapp/ui/HomeFragment.kt
+++ app/src/main/java/com/livrokotlin/taskapp/ui/HomeFragment.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.livrokotlin.taskapp.R
 import com.livrokotlin.taskapp.databinding.FragmentHomeBinding
+import com.livrokotlin.taskapp.extensions.showBottomSheet
 import com.livrokotlin.taskapp.ui.adapter.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
 
         initTabs()
         logout()
@@ -50,7 +56,15 @@ class HomeFragment : Fragment() {
 
     private fun logout() {
         binding.btnLogout.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_authentication)
+            showBottomSheet(
+                title = R.string.txt_title_dialog_confirm_logout,
+                textBottom = R.string.txt_btn_dialog_confirm_logout,
+                message = getString(R.string.txt_message_dialog_confirm_logout),
+                onClick = {
+                    auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_authentication)
+                }
+            )
         }
     }
 
